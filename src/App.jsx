7f0c8671e4ad3373dc43ee4a68846e3b33e0086e
sarthak <sarthak613singh@gmail.com>
2024-09-29 16:52:44 +0530

    refactor: Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,12 @@
 import "./App.css";
-import Hero from "./component/Hero/Hero";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Services from "./component/Services/Services";
-import ContactForm from "./component/ContactForm/ContactForm";
-import Footer from "./component/Footer/Footer";
-import TripCalculator from "./component/TripCalculator/TripCalculator";
-import ImageSlider from "./component/carousel/ImageSlider";
-import HappyCustomers from "./component/HappyCustomers/HappyCustomers";
-import HoneymoonDesties from "./component/HoneyMoondesties/HoneymoonDesties";
-import Testimonial from "./component/testimonial/testimonial";
-import TrandingSlider from "./component/TrandingSlider/TrandingSlider";
-
+import HomePage from "./Pages/HomePage/HomePage";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Hero />
-                <Services></Services>
-                <ImageSlider />
-                {/* <TripCalculator />  */}
-                <HappyCustomers />
-                <Testimonial />
-                <ContactForm />
-                <HoneymoonDesties />
-                <TrandingSlider />
-                <Footer></Footer>
-              </>
-            }
-          />
+          <Route path="/" element={<HomePage />} />
           <Route path="/personalised-trips" element={<div />} />
           <Route path="/corporate-trips" element={<div />} />
           <Route path="/group-trips" element={<div />} />
